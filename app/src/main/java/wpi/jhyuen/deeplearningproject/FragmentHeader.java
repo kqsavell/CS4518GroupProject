1b4f0aeab32e643765d7e251e69341d119e9379c
Joseph Yuen <josephnewbox@gmail.com>
2018-12-08 13:52:28 -0500

    moved around upload button and rearranged the add onClickListener to the button. also added new upload icon

--- app/src/main/java/wpi/jhyuen/deeplearningproject/FragmentHeader.java
+++ app/src/main/java/wpi/jhyuen/deeplearningproject/FragmentHeader.java
@@ -6,14 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class FragmentHeader extends Fragment
 {
+
+    private MainActivity parent = null;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+
         View view = inflater.inflate(R.layout.fragment_header, container, false);
+
+        // Get parent activity
+        parent = (MainActivity)getActivity();
+
+        ImageButton emailBtn = (ImageButton)view.findViewById(R.id.driveButton);
+        emailBtn.setOnClickListener(parent.emailListener);
+
         return view;
+
     }
 
 }
